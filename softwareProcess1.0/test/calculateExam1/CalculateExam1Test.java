cd19d3cc5d0499909d9856fd819e94918fd97693
118371030761 <825987405@qq.com>
2020-11-06 18:24:25 +0800

    first commit

---
+++ softwareProcess1.0/test/calculateExam1/CalculateExam1Test.java
@@ -0,0 +1,14 @@
+package calculateExam1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculateExam1Test {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
